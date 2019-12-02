4bfa7f6f03ebb67a2a884b576204964f8fab7672
149alexniu149 <alexniu149@gmail.com>
2019-12-01 23:40:19 -0500

    add initial bootstrap script

---
+++ bootstrap_scripts/singlenode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+
+yum install docker-engine -y
+
+service docker start
+
+docker run hello-world
+
+systemctl enable docker
