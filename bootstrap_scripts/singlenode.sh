ddd77836380c9aaa76827e2017d75327ec33f2f8
root <root@ip-172-31-19-219.ec2.internal>
2019-12-02 05:38:34 +0000

    install kubectl

--- bootstrap_scripts/singlenode.sh
+++ bootstrap_scripts/singlenode.sh
@@ -21,3 +21,5 @@ docker build -t kubia ../ -f ../Dockerfile
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.23.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/bin/
 
 minikube start --vm-driver=none
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/bin/
